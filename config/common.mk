02bf6ae24434cf177e30461d8531e44855b2b95f
Toufu <hamm157famm7423@gmail.com>
2026-05-21 05:54:07 +0000

    config: Include WitAqua config
    Change-Id: Iadd0d12f1efa5355d2b0c597c00822d073b4ce84

--- config/common.mk
+++ config/common.mk
@@ -311,3 +311,6 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+# Include WitAqua config
+include vendor/witaqua/config/common.mk
